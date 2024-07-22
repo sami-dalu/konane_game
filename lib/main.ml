0f0368341341fb6a65a32a1b0379a6f34fc42acf
sami-dalu <ubuntu@ip-172-31-20-10.us-east-2.compute.internal>
2024-07-22 19:42:34 +0000

    initial implementation of available_captures_for_player

--- lib/main.ml
+++ lib/main.ml
@@ -33,7 +33,8 @@ module Exercises = struct
       | None -> if (in_board_range pos_two_below game) then Some {Move.starting_pos = pos; ending_pos = Some pos_two_below} else None
     )
 else None
-  ) in
+  )
+in
   let capture_right = (
     let piece_right = Map.find game.board right_my in
     match piece_right with 
@@ -48,11 +49,45 @@ else None
     )
 else None
   )
-in []
+in let capture_left = (
+  let piece_left = Map.find game.board left_my in
+  match piece_left with 
+  (* None => no piece to capture *)
+  | None -> None
+  | Some piece -> if (Piece.equal piece opp_piece) then (
+    let pos_two_left = {Position.row = pos.row; column = left_my.column - 1} in
+    match Map.find game.board pos_two_left with 
+    (* some => you can't jump to there *)
+    | Some _ -> None 
+    | None -> if (in_board_range pos_two_left game) then (Some {Move.starting_pos = pos; ending_pos = Some pos_two_left}) else None
+  )
+else None
+)
+in
+let capture_above = (
+  let piece_above = Map.find game.board above_my in
+  match piece_above with 
+  (* None => no piece to capture *)
+  | None -> None
+  | Some piece -> if (Piece.equal piece opp_piece) then (
+    let pos_two_above = {Position.row = above_my.row - 1; column = pos.column} in
+    match Map.find game.board pos_two_above with 
+    (* some => you can't jump to there *)
+    | Some _ -> None 
+    | None -> if (in_board_range pos_two_above game) then (Some {Move.starting_pos = pos; ending_pos = Some pos_two_above}) else None
+  )
+else None
+)
+in
+let potential_moves = [capture_above; capture_below; capture_left; capture_right] in
+let move_opts = List.filter potential_moves ~f:(fun potential_move -> Option.is_some potential_move) in
+match move_opts with 
+| [] -> []
+| move_list -> List.map move_list ~f:(fun move_opt -> Option.value_exn move_opt)
 
 ;;
   let new_game = Game.new_game ~height:8 ~width:8
-  let available_moves (game : Game.t) ~(my_piece : Piece.t) : Move.t list =
+  let available_captures_for_player (game : Game.t) ~(my_piece : Piece.t) : Move.t list =
     match game.game_state with 
     | First_moves -> 
       let top_left = {Position.row = 0; column = 0} in
@@ -74,18 +109,12 @@ in []
       let adjacent_positions_on_board = List.filter adjacent_positions ~f:(fun pos -> in_board_range pos game) in
       List.map adjacent_positions_on_board ~f:(fun valid_pos -> {Move.starting_pos = valid_pos; ending_pos = None})
     )
-    | _ -> 
-    let rows = List.range 0 len in
-    let cols = List.range 0 len in
-    let positions_grid =
-      List.map rows ~f:(fun r ->
-        List.map cols ~f:(fun c -> {Position.row = r; column = c }))
-    in
-    let positions_list = List.concat positions_grid in
-    List.filter positions_list ~f:(fun pos ->
-      not
-        (List.exists (Map.keys game.board) ~f:(fun p ->
-           Position.equal p pos)))
+    | _ -> List.fold (Map.keys game.board) ~init:[] ~f:(fun captures_so_far pos -> 
+      let piece_at_pos =  (Map.find_exn game.board pos) in
+      if (Piece.equal piece_at_pos my_piece) then (
+        let available_captures = possible_captures_from_occupied_pos_exn game pos in
+        available_captures @ captures_so_far) else captures_so_far
+      )
 
   ;;
 
