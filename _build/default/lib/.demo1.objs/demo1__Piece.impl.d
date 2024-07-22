a7660af8415d05613f3e623966a30e1aabea69ab
sami-dalu <ubuntu@ip-172-31-20-10.us-east-2.compute.internal>
2024-07-22 14:42:09 +0000

    hopefully merged correctly

---
+++ _build/default/lib/.demo1.objs/demo1__Piece.impl.d
@@ -0,0 +1,1 @@
+lib/piece.pp.ml: Core Fn Ppx_inline_test_lib Sexp
---
+++ _build/default/lib/piece.ml
@@ -0,0 +1,16 @@
+open! Core
+
+module Piece = struct
+  type t =
+    | X
+    | O
+  [@@deriving sexp, equal, compare, bin_io, enumerate]
+
+  let of_string = Fn.compose t_of_sexp Sexp.of_string
+  let to_string = Fn.compose Sexp.to_string_hum sexp_of_t
+
+  let flip = function
+    | X -> O
+    | O -> X
+  ;;
+end
